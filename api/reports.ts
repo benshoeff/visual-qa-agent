5590f3061029583d0821adb4e6eb522b5550057d
benshoeff <benshoeff@gmail.com>
2026-08-28 12:57:32 +0300

    fix vercel 3

--- api/reports.ts
+++ api/reports.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { listDirectory, corsHeaders } from "../serverless/github";
+import { listDirectory, corsHeaders } from "./_lib/github";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === "OPTIONS") {
